8a3d9b772fe49d682532283c09043b029a94e6a5
KaijianHuang <112913171+KaijianHuang@users.noreply.github.com>
2023-04-12 00:10:16 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
 use std::io;
 
 fn main() {
-    println!("Enter the name of a popular city(new york city, london, paris, tokyo):");
-    let mut city = String::new();
-    io::stdin().read_line(&mut city).expect("Failed to read input.");
+    // Read in the user's array
+    let mut input = String::new();
+    println!("Enter a space-separated list of numbers:");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let arr: Vec<i32> = input.trim().split_whitespace().map(|x| x.parse().unwrap()).collect();
 
-    match city.trim().to_lowercase().as_str() {
-        "new york city" => println!("New York City is the largest city in the United States, with a population of over 8 million people. It is a global center for finance, media, art, and culture."),
-        "london" => println!("London is the capital and largest city of England and the United Kingdom, with a population of over 8 million people. It is a global center for finance, education, healthcare, media, and the arts."),
-        "paris" => println!("Paris is the capital and largest city of France, with a population of over 2 million people. It is known as the City of Light, and is famous for its art, fashion, cuisine, and historic landmarks such as the Eiffel Tower and the Louvre Museum."),
-        "tokyo" => println!("Tokyo is the capital and largest city of Japan, with a population of over 13 million people. It is a global center for finance, technology, entertainment, and fashion."),
-        _ => println!("Sorry, I don't have an introduction for that city."),
+    // Read in the user's target number
+    let mut input = String::new();
+    println!("Enter the target number:");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let target: i32 = input.trim().parse().unwrap();
+
+    // Perform binary search on the array
+    let mut left = 0;
+    let mut right = arr.len() - 1;
+    while left <= right {
+        let mid = (left + right) / 2;
+        if arr[mid] == target {
+            println!("Found the target number at index {}!", mid);
+            return;
+        } else if arr[mid] < target {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
+
+    println!("The target number was not found in the array.");
 }
